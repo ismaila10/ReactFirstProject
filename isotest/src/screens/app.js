7bfa4b89ab28ce75cf9977b3602230a93208a6e0
ismaila <lodia.ismaila@gmail.com>
2020-12-10 11:31:32 +0100

    Organisation du projet et création d'un composant

---
+++ isotest/src/screens/app.js
@@ -0,0 +1,16 @@
+import logo from '../assets/logo.svg';
+import './app.css';
+import Header from '../components/header';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Header > < /Header>
+
+        <
+        /div>
+    );
+}
+
+export default App;
