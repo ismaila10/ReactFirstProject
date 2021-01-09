98f0e94f36ea7b61d3d953cd2ebac7f551479560
Ismaila Diallo <lodia.ismaila@gmail.com>
2021-01-09 14:43:25 +0100

    React vers PWA

--- isotest/src/components/header/index.js
+++ isotest/src/components/header/index.js
@@ -1,24 +1,28 @@
 import React from 'react'
-import logo from '../../assets/logo.svg';
-import './index.css';
-import PropTypes from 'prop-types';
+import logo from '../../assets/logo.svg'
+import './index.css'
+import PropTypes from 'prop-types'
 
-const Header = (props) => {
-    //console.log(props)
-    return ( 
-        <header className = "App-header" >
-            <img src = { logo } className = "App-logo" alt = "logo" />
-            <p > { props.label } </p> 
-            <a className = "App-link" href = "https://reactjs.org" target = "_blank" rel = "noopener noreferrer" >
-                Learn React 
-            </a> 
-        </header>
-    )
+const Header = props => {
+  //console.log(props)
+  return (
+    <header className='App-header'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <p> {props.label} </p>
+      <a
+        className='App-link'
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn React
+      </a>
+    </header>
+  )
 }
 
-
 Header.propTypes = {
-    label: PropTypes.string
+  label: PropTypes.string
 }
 
 export default Header
